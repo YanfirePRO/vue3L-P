fa752ad2a54cd997bdc4f279abf5ec030f92e880
yanchengjie <yanchengjie2011@163.com>
2022-06-24 18:01:17 +0800

    响应系统添加cleanUp

--- untils/vue-watchdata.js
+++ untils/vue-watchdata.js
@@ -24,11 +24,13 @@ function effect(fn){
         activeEffect = effectFn;
         fn()
     }
-    
+    effectFn.deps = []
+    effectFn()
 }
 
 //- 抽离track和trigger函数
 function track(target, property){
+    console.log("track-------", property)
     if(!activeEffect){
         return 
     }
@@ -41,19 +43,28 @@ function track(target, property){
         depsMap.set(property, deps = new Set())
     }
     deps.add(activeEffect)
+    //- 收集与activeEffect相关的依赖
+    activeEffect.deps.push(deps)
 }
 
 //- 分支切换与cleanup
-function cleanup(){
-
+function cleanup(effect){
+    let deps = effect.deps;
+    for(let i=0;i<deps.length;i++){
+        let depsSet = deps[i];
+        depsSet.delete(effect)
+    }
+    effect.deps.length = 0;
 }
 
 function trigger(target, property){
+    console.log("trigger-------", property)
     let depsMap = bucket.get(target);
     if(!depsMap){
         return 
     }
-    let deps = depsMap.get(property);
+    //- 新建Set 避免forEach时无限循环
+    let deps = new Set(depsMap.get(property));
     deps && deps.forEach(fn => fn())
 }
 
@@ -74,6 +85,13 @@ effect(()=>{
 })
 
 setTimeout(()=>{
-    console.log(`重新设置` + testdata.value)
+    dataProxy.ok = false;
     dataProxy.value = 200;
+    console.log(bucket, 'bucket')
 }, 2000)
+
+// setTimeout(()=>{
+//     dataProxy.value = 400;
+//     console.log(`重新设置`)
+//     console.log(bucket, 'bucket')
+// }, 4000)
